66e6818fb7bd7d1cd681a4e3d0b11b7faad66b13
chenshaojun <chenshaojun@cecdat.com.cn>
2024-11-08 15:56:17 +0800

    插件图标可用性限制。

--- src/background/background.js
+++ src/background/background.js
@@ -9,11 +9,23 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.url) {
-        if (tab.url.indexOf('timetable') > 0) {
-            if (tab.status === 'loading') {
-                chrome.tabs.sendMessage(tabId, { type: 'inject', tab: tab }, function (response) {
-                });
+        if (tab.url.indexOf('talk915') > 0) {
+            chrome.action.enable(tabId);
+            if (tab.url.indexOf('timetable') > 0 && tab.status === 'loading') {
+                chrome.tabs.sendMessage(tabId, { type: 'inject', tab: tab });
             }
+        } else {
+            chrome.action.disable(tabId);
         }
     }
-})
+});
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+    let tabId = activeInfo.tabId;
+    const tab = await chrome.tabs.get(tabId);
+    if (tab.url.indexOf('talk915') > 0) {
+        chrome.action.enable(tabId);
+    } else {
+        chrome.action.disable(tabId);
+    }
+});
